9cc3a2e7445cbb001d34d9857163e11ad14920c2
Junaid Bashir <47460568+Junaid-Bashir@users.noreply.github.com>
2023-07-27 20:49:38 +0500

    kjhkhj

--- main.py
+++ main.py
@@ -1,48 +1,144 @@
-import subprocess
+from glob import glob
+import shutil
+import torch
+from time import strftime
+import os, sys, time
+from argparse import ArgumentParser
 from fastapi import FastAPI
-from fastapi import FastAPI, UploadFile, File
-from fastapi.staticfiles import StaticFiles
-from gtts import gTTS
-from io import BytesIO
+from pydantic import BaseModel
+
+from src.utils.preprocess import CropAndExtract
+from src.test_audio2coeff import Audio2Coeff  
+from src.facerender.animate import AnimateFromCoeff
+from src.generate_batch import get_data
+from src.generate_facerender_batch import get_facerender_data
+from src.utils.init_path import init_path
+
 app = FastAPI()
 
-async def run_inference(driven_audio_path, source_image_path, result_dir='static'):
-    try:
-        command = f"python inference.py --driven_audio {driven_audio_path} \
-                   --source_image {source_image_path} \
-                   --result_dir {result_dir} --still --preprocess full --enhancer gfpgan"
+# Define the FastAPI model for the function parameters
+class ProcessInput(BaseModel):
+    driven_audio: "static/test.wav"
+    source_image: "static/1669376551115.jpg"
+    ref_eyeblink: str = None
+    ref_pose: str = None
+    checkpoint_dir: str = './checkpoints'
+    result_dir: str = './static'
+    pose_style: int = 0
+    batch_size: int = 2
+    size: int = 256
+    expression_scale: float = 1.0
+    input_yaw: list = None
+    input_pitch: list = None
+    input_roll: list = None
+    enhancer: str = "gfpgan "
+    background_enhancer: str = None
+    cpu: bool = False
+    face3dvis: bool = False
+    still: bool = True
+    preprocess: str = 'full'
+    verbose: bool = False
+    old_version: bool = False
+    net_recon: str = 'resnet50'
+    init_path: str = None
+    use_last_fc: bool = False
+    bfm_folder: str = './checkpoints/BFM_Fitting/'
+    bfm_model: str = 'BFM_model_front.mat'
+    focal: float = 1015.0
+    center: float = 112.0
+    camera_d: float = 10.0
+    z_near: float = 5.0
+    z_far: float = 15.0
 
-        # Run the command and capture the response
-        response = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    class Config:
+        arbitrary_types_allowed = True
+def main(args):
+    #torch.backends.cudnn.enabled = False
 
-        # Decode the response bytes to a string
-        response_str = response.decode('utf-8')
+    pic_path = args.source_image
+    audio_path = args.driven_audio
+    save_dir = os.path.join(args.result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
+    os.makedirs(save_dir, exist_ok=True)
+    pose_style = args.pose_style
+    device = args.device
+    batch_size = args.batch_size
+    input_yaw_list = args.input_yaw
+    input_pitch_list = args.input_pitch
+    input_roll_list = args.input_roll
+    ref_eyeblink = args.ref_eyeblink
+    ref_pose = args.ref_pose
 
-        return response_str
-    except subprocess.CalledProcessError as e:
-        # If there is an error, you can handle it here
-        print("Error occurred:", e)
-        return None
-app.mount("/static", StaticFiles(directory="static"), name="static")
+    current_root_path = os.path.split(sys.argv[0])[0]
 
-def text_to_audio(text, audio_path):
-    tts = gTTS(text=text, lang="en")
-    tts.save(audio_path)
+    sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
 
-@app.get("/run_inference/")
-async def call_run_inference(text:str, image: UploadFile = File(...)):
+    #init model
+    preprocess_model = CropAndExtract(sadtalker_paths, device)
+
+    audio_to_coeff = Audio2Coeff(sadtalker_paths,  device)
     
-    # # Save the uploaded files to a temporary directory
-    # name = image.filename
-    # contents = image.file.read()
-    # with open("static/"+name,'wb') as data:
-    #     data.write(contents)
-    #     data.close()
-    # text_to_audio(text, "static/test.mp3")
+    animate_from_coeff = AnimateFromCoeff(sadtalker_paths, device)
 
-    response = await run_inference("static/test.wav", "static/1669376551115.jpg", result_dir='static')
+    #crop image and extract 3dmm from image
+    first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
+    os.makedirs(first_frame_dir, exist_ok=True)
+    print('3DMM Extraction for source image')
+    first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
+                                                                             source_image_flag=True, pic_size=args.size)
+    if first_coeff_path is None:
+        print("Can't get the coeffs of the input")
+        return
 
-    if response is not None:
-        return {"response": response.strip().split("\n")[-1].split(":")[-1]}
+    if ref_eyeblink is not None:
+        ref_eyeblink_videoname = os.path.splitext(os.path.split(ref_eyeblink)[-1])[0]
+        ref_eyeblink_frame_dir = os.path.join(save_dir, ref_eyeblink_videoname)
+        os.makedirs(ref_eyeblink_frame_dir, exist_ok=True)
+        print('3DMM Extraction for the reference video providing eye blinking')
+        ref_eyeblink_coeff_path, _, _ =  preprocess_model.generate(ref_eyeblink, ref_eyeblink_frame_dir, args.preprocess, source_image_flag=False)
     else:
-        return {"error": "An error occurred during inference or no non-empty lines in the response."}
+        ref_eyeblink_coeff_path=None
+
+    if ref_pose is not None:
+        if ref_pose == ref_eyeblink: 
+            ref_pose_coeff_path = ref_eyeblink_coeff_path
+        else:
+            ref_pose_videoname = os.path.splitext(os.path.split(ref_pose)[-1])[0]
+            ref_pose_frame_dir = os.path.join(save_dir, ref_pose_videoname)
+            os.makedirs(ref_pose_frame_dir, exist_ok=True)
+            print('3DMM Extraction for the reference video providing pose')
+            ref_pose_coeff_path, _, _ =  preprocess_model.generate(ref_pose, ref_pose_frame_dir, args.preprocess, source_image_flag=False)
+    else:
+        ref_pose_coeff_path=None
+
+    #audio2ceoff
+    batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=args.still)
+    coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+
+    # 3dface render
+    if args.face3dvis:
+        from src.face3d.visualize import gen_composed_video
+        gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
+    
+    #coeff2video
+    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
+                                batch_size, input_yaw_list, input_pitch_list, input_roll_list,
+                                expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size)
+    
+    result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info, \
+                                enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
+    
+    shutil.move(result, save_dir+'.mp4')
+    print('The generated video is named:', save_dir+'.mp4')
+
+@app.post("/process/")
+def process_data(input_data: ProcessInput):
+    args = input_data.dict()
+
+    if torch.cuda.is_available() and not args['cpu']:
+        args['device'] = "cuda"
+    else:
+        args['device'] = "cpu"
+
+    # Call the main function with the arguments provided in the input_data
+    main(args)
+    return {"message": "Processing completed. The generated video has been saved."}
